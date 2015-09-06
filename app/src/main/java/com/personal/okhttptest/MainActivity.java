2963bb86a3db1ffd60b9dfd3779f0c8bdd758f6b
ansel <anselhall@163.com>
2015-09-06 22:48:51 +0800

    Initial commit

---
+++ app/src/main/java/com/personal/okhttptest/MainActivity.java
@@ -0,0 +1,74 @@
+package com.personal.okhttptest;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.main_iv)
+    ImageView main_iv;
+    @InjectView(R.id.main_tv)
+    TextView main_tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        initData();
+    }
+
+    private void initData() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request build = new Request.Builder().url("http://img.my.csdn.net/uploads/201508/05/1438760726_5120.jpg").build();
+        Call call = okHttpClient.newCall(build);
+        call.enqueue(new Callback() {
+
+            private Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
+            private String string = "Hello";
+
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.e("Log", "FAILURE");
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                InputStream inputStream = response.body().byteStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+
+                /*string = response.body().string();
+                Log.e("Log", string + "SUCCESS");*/
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        main_iv.setImageBitmap(bitmap);
+                        main_tv.setText(string);
+                    }
+                });
+            }
+        });
+    }
+}
